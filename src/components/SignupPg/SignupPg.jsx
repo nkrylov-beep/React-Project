9c623ccb32d14a9693ec2269ebbf06a9d441aeb9
nkrylov-beep <Nikita1998k@mail.ru>
2019-12-09 22:20:26 +0300

    sdelol stranitcu logina na primitivnom urovne

--- src/components/SignupPg/SignupPg.jsx
+++ src/components/SignupPg/SignupPg.jsx
@@ -4,7 +4,7 @@ import classes from './SignupPg.module.css';
 const SignupPg = () => {
     return (
         <div className={classes.container}>
-            <div className={classes.inscription}><p className={classes.p2}>Registration</p></div>
+            <div className={classes.inscription}><p className={classes.p2}>REGISTRATION</p></div>
             <div className={classes.nickField}><input /></div>
             <div className={classes.passwordField}><input /></div>
             <div className={classes.passwordField2}><input /></div>
