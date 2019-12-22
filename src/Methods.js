5ee9078679918c169ce49b1eabeb6599223ec597
Anton Kurvatov <a.kurbatov@protonmail.com>
2019-12-22 19:56:29 +0300

    change registration

--- src/Methods.js
+++ src/Methods.js
@@ -8,8 +8,11 @@ export async function registration(e) {
     const password = e.target.elements.password.value;
     const password2 = e.target.elements.password2.value;
     console.log("111");
-    const api_url = await fetch('http://hehmda.herokuapp.com:8000/api/v1/users/registration', {
+    const api_url = await fetch('http://hehmda.herokuapp.com/api/v1/users/registration', {
         method: 'POST',
+        headers: {
+           'Content-Type': 'application/json;charset=utf-8'
+        },
         body: `{\"new_nickname\":\"${name}\", \"new_login\":\"${login}\", \"new_password\": \"${password}\", \"new_repeat_password\": \"${password2}\"}`
     });
     const data = await api_url.json();
