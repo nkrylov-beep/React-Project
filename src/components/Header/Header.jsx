934b71d4889167ea76a5302e3e51a11c12e75755
nkrylov-beep <Nikita1998k@mail.ru>
2019-12-25 02:41:46 +0300

    fix header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,28 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {Navbar, Nav, Button} from "react-bootstrap";
+import { Navbar, Nav, Button } from "react-bootstrap";
+import classes from './Header.module.css';
+import { NavLink } from "react-router-dom";
 
 class Header extends React.Component {
     render() {
         return (
             <Navbar bg="dark" variant="dark">
-                <Navbar.Brand>MonoChrome</Navbar.Brand>
+                <Navbar.Brand>
+                    <NavLink className={classes.link} to='/'>
+                        Nazvanie
+            </NavLink>
+                </Navbar.Brand>
                 <Nav className="mr-auto">
                     <Button variant="outline-dark" href={'/donate'}>Donate</Button>
                 </Nav>
                 <Nav>
-                    <Button variant="dark" href={'/login'}>Login</Button>
-                    <Button variant="dark" href={'/signup'}>SignUp</Button>
+                    <NavLink to="/signup">
+                        <Button variant="dark">SignUp</Button>
+                    </NavLink>
+                    <NavLink to="/login">
+                        <Button variant="dark">Login</Button>
+                    </NavLink>
                 </Nav>
             </Navbar>
         );
