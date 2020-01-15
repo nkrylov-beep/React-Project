f39884627dd2afe8b95e5cbb7dfa92c50a33d005
idee-fixe <my3feed@gmail.com>
2020-01-15 17:34:48 +0300

    change layout of signup page

--- src/components/SignupPg/Form/Form.jsx
+++ src/components/SignupPg/Form/Form.jsx
@@ -20,30 +20,18 @@ class Form extends React.Component {
     }
     render() {
         return (
-            <div className={classes.wrapper}>
-                <div className={classes.inscr}>
-                    <div className={classes.nickInscr}><p className={classes.p}>имя:</p></div>
-                    <div className={classes.gap2}></div>
-                    <div className={classes.loginInscr}><p className={classes.p}>логин:</p></div>
-                    <div className={classes.gap2}></div>
-                    <div className={classes.passwordInscr}><p className={classes.p}>пароль:</p></div>
-                    <div className={classes.gap2}></div>
-                    <div className={classes.passwordInscr2}><p className={classes.p}>повторите пароль:</p></div>
-                    <div className={classes.gap2}></div>
-                </div>
-                <form className={classes.form} onSubmit={this.handleClick}>
-                    <input type='text' name='name' />
-                    <div className={classes.gap}></div>
-                    <input type='text' name='login' />
-                    <div className={classes.gap}></div>
-                    <input type="password" name='password' />
-                    <div className={classes.gap}></div>
-                    <input type="password" name='password2' />
-                    <div className={classes.gap}></div>
-                    <button className={classes.button}>готово</button>
-                    <div className={classes.status}>{this.state.status}</div>
-                </form>
-            </div>
+            <form className={classes.form} onSubmit={this.handleClick}>
+                <input type='text' name='name' placeholder="Имя"/>
+                <div className={classes.gap}></div>
+                <input type='text' name='login' placeholder="Логин"/>
+                <div className={classes.gap}></div>
+                <input type="password" name='password' placeholder="Пароль"/>
+                <div className={classes.gap}></div>
+                <input type="password" name='password2' placeholder="Повторите пароль"/>
+                <div className={classes.gap}></div>
+                <button className={classes.button}>готово</button>
+                <div className={classes.status}>{this.state.status}</div>
+            </form>
         );
     }
 }
